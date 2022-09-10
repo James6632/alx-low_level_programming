fe6670bdfc5ea5879ff8aedb4e864f5db1ad9588
James6632 <jkkwakye66@gmail.com>
2022-09-09 23:58:56 -0700

    finish

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,19 +7,13 @@
  */
 int	main(void)
 {
+	int	n;
 	char	c;
 
-	for	(c	=	'0';	c	<=	'f';	c++)
-	{
-		putchar(c);
-	}
-
+	for	(n	=	0;	n	<	10;	n++)
+		putchar((n	%	10)	+	'0');
 	for	(c	=	'a';	c	<=	'f';	c++)
-	{
 		putchar(c);
-	}
-
 	putchar('\n');
-
 	return	(0);
 }
