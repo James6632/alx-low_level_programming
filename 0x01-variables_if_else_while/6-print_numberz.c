5b898d46e05b1a704d1bfca521421adfda23e8b2
James6632 <jkkwakye66@gmail.com>
2022-09-11 05:01:16 -0700

    foolish girl

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,19 @@
 /**
  * main	-	prints	the	numbers	from	0	to	9
  *
- * Return:	Always	0	(success)
+ * Description: Using the main function
+ * this program prints "Programming is positive, zero or negative
+ * Return: 0
  */
-int	main(void)
+int main(void)
 {
-	int	n;
 
-	for	(n	=	0;	<	10;	n++)
-	putchar((n	%	10)	+	'0');
+	int n;
 
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
 	putchar('\n');
-
-	return	(0);
+	return (0);
 }
