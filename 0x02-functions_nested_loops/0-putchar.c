ddae4489960281e36a126969c20ab2c16f693a2c
James6632 <jkkwakye66@gmail.com>
2022-09-13 13:20:49 -0700

    bite

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
+}
